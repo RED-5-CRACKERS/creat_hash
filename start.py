4cbea6395b9ad42cf09bb55b423bea41fa74f863
M҉ a҉ d҉ h҉ a҉ v҉ a҉ <57581347+Madhava-mng@users.noreply.github.com>
2020-02-21 10:15:08 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 #
 #
+print("                        🇭 🇦 🇸 🇭 -🇨 🇷 🇪 🇦 🇹 🇪 🇷                                               ")
 #crear a hases 
 az='y'
 while az is 'y':
@@ -16,18 +17,17 @@ while az is 'y':
             elif a == 'exit':
                 b=a
                 break
-            elif a == 'help':
-                print("sha1    : moves sha1 hash folder")
-                print('md5    : moves md5 hash folder')
+            elif a == '?':
+                print("sha1,sha224,sha256,sha384  ")
+                print('sha512 : algorithm   ')
                 print('..     : go back')
                 print('get    : ready to get the input')
-                print('help/? : this banner will appear ')
+                print('?      : this banner will appear ')
                 print("exit   : exit")
             elif a == '':
                 print('Hash/md5/:> ')
             else:
-                print("Error: no 'get' specefied")
-                print('use get comment')
+                print("Error: 'get' unspecefied, use: ?")
     elif b == 'sha1':
         while b == 'sha1':
             a=input('Hash/sha1/:> ')
@@ -38,31 +38,111 @@ while az is 'y':
                 break
             elif a == 'exit':
                 break
-            elif a == 'help':
-                print("sha1    : moves sha1 hash folder")
-                print('md5    : moves md5 hash folder')
+            elif a == '?':
+                print("sha1,sha224,sha256,sha384  ")
+                print('sha512 : algorithm   ')
                 print('..     : go back')
                 print('get    : ready to get the input')
-                print('help/? : this banner will appear ')
+                print('?      : this banner will appear ')
                 print("exit   : exit")
             elif a == '':
                 print('Hash/sha1/:> ')
             else:
-                print("Error: 'get' unspecefied ")
+                print("Error: 'get' unspecefied, use ? ")
     elif b == 'exit':
         break
-    elif b == 'help':
-        print("sha1    : moves sha1 hash folder")
-        print('md5    : moves md5 hash folder')
-        print('..     : go back')
-        print('get    : ready to get the input')
-        print('help/? : this banner will appear ')
-        print("exit   : exit")
+    elif a == '?':
+                print("sha1,sha224,sha256,sha384  ")
+                print('sha512 : algorithm   ')
+                print('..     : go back')
+                print('get    : ready to get the input')
+                print('?      : this banner will appear ')
+                print("exit   : exit")
     elif b == '':
         print('Hash/:> ')
 
     else:
-        print()
-        print()
-        print('Invalid  !^        operator.')
-        print('See at help or ? .' )
+        print("Error: 'get' unspecified , use ?")
+    elif b == 'sha224':
+        while b == 'sha224':
+            a=input('Hash/sha224/:> ')
+            if a == 'get':
+                c=input('Hash/sha224/getting:> ')
+                print('Hash/sha224:> ',hashlib.sha224(c.encode('UTF-8')).hexdigest())
+            elif a == '..':
+                break
+            elif a == '':
+                print('Hash/sha224:> ')
+            elif a == '?':
+                print("sha1,sha224,sha256,sha384  ")
+                print('sha512 : algorithm   ')
+                print('..     : go back')
+                print('get    : ready to get the input')
+                print('?      : this banner will appear ')
+                print("exit   : exit")
+            else:
+                print('Error : 'get' unspecified ,use ? ') 
+         
+    elif b == 'sha256':
+        while b == 'sha256':
+            a=input('Hash/sha256/:> ')
+            if a == 'get':
+                c=input('Hash/sha256/getting:> ')
+                print('Hash/sha256/:> ',hashlib.sha265(c.encode('UTF-8')).hexdigest())
+            elif a == '..':
+                break
+            elif a == 'exit':
+                break
+            elif a == '?':
+                print("sha1,sha224,sha256,sha384  ")
+                print('sha512 : algorithm   ')
+                print('..     : go back')
+                print('get    : ready to get the input')
+                print('?      : this banner will appear ')
+                print("exit   : exit")
+            elif a == '':
+                print('Hash/sha256/:> ')
+            else:
+                print("Error: 'get' unspecefied, use : ? ")
+    elif b == 'sha384':
+        while b == 'sha384':
+            a=input('Hash/sha384/:> ')
+            if a == 'get':
+                c=input('Hash/sha384/getting:> ')
+                print('Hash/sha384/:> ',hashlib.sha384(c.encode('UTF-8')).hexdigest())
+            elif a == '..':
+                break
+            elif a == 'exit':
+                break
+            elif a == '?':
+                print("sha1,sha224,sha256,sha384  ")
+                print('sha512 : algorithm   ')
+                print('..     : go back')
+                print('get    : ready to get the input')
+                print('?      : this banner will appear ')
+                print("exit   : exit")
+            elif a == '':
+                print('Hash/sha384/:> ')
+            else:
+                print("Error: 'get' unspicified , use: ? ")
+    elif b == 'sha512':
+        while b == 'sha512':
+            a=input('Hash/sha512/:> ')
+            if a == 'get':
+                c=input('Hash/sha512/getting:> ')
+                print('Hash/sha512/:> ',hashlib.sha512(c.encode('UTF-8')).hexdigest())
+            elif a == '..':
+                break
+            elif a == 'exit':
+                break
+            elif a == '?':
+                print("sha1,sha224,sha256,sha384  ")
+                print('sha512 : algorithm   ')
+                print('..     : go back')
+                print('get    : ready to get the input')
+                print('?      : this banner will appear ')
+                print("exit   : exit")
+            elif a == '':
+                print('Hash/sha512/:> ')
+            else:
+                print("Error: 'get' unspecified, use : ? ")
